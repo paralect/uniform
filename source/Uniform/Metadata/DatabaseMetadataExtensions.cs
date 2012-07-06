1d711d0840282164ac7a7a0b7b886317efdad2d7
Dmitry Schetnikovich <tschetnikovich@gmail.com>
2012-07-06 21:05:21 +0300

    DatabaseMetadata scan for types with CollectionAttribute implemented

--- source/Uniform/Metadata/DatabaseMetadataExtensions.cs
+++ source/Uniform/Metadata/DatabaseMetadataExtensions.cs
@@ -1,19 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Uniform.Attributes;
 
 namespace Uniform
 {
     public static class DatabaseMetadataExtensions
     {
-         public static DatabaseMetadataConfiguration AddDocumentType<TDocument>(this DatabaseMetadataConfiguration configuration)
-         {
-             configuration.DocumentTypes.Add(typeof (TDocument));
-             return configuration;
-         }
+        public static DatabaseMetadataConfiguration AddDocumentType<TDocument>(this DatabaseMetadataConfiguration configuration)
+        {
+            configuration.DocumentTypes.Add(typeof (TDocument));
+            return configuration;
+        }
 
-         public static DatabaseMetadataConfiguration AddDocumentType(this DatabaseMetadataConfiguration configuration, Type documentType)
-         {
-             configuration.DocumentTypes.Add(documentType);
-             return configuration;
-         }
+        public static DatabaseMetadataConfiguration AddDocumentType(this DatabaseMetadataConfiguration configuration, Type documentType)
+        {
+            configuration.DocumentTypes.Add(documentType);
+            return configuration;
+        }
+
+        public static DatabaseMetadataConfiguration AddDocumentTypes(this DatabaseMetadataConfiguration configuration, Assembly assembly, String fullNameStartsFrom = null)
+        {
+            var result = GetTypesWithAttribute<CollectionAttribute>(new[] { assembly });
+
+            if (fullNameStartsFrom != null)
+                result = result
+                    .Where(t => t.FullName != null && t.FullName.StartsWith(fullNameStartsFrom));
+
+            configuration.DocumentTypes.AddRange(result);
+            return configuration;
+        }
+
+        private static IEnumerable<Type> GetTypesWithAttribute<TAttribute>(params Assembly[] assemblies)
+        {
+            var typesWithAttribute =
+                from a in assemblies
+                from t in a.GetTypes()
+                let attributes = t.GetCustomAttributes(typeof (TAttribute), true)
+                where attributes != null && attributes.Length > 0 
+                select t;
+
+            return typesWithAttribute;
+        }
     }
 }
